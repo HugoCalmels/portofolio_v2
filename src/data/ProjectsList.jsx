f59eb6e920f7982a283c0a040fd2a46d8d0d4796
Hugo Calmels <calmels.hugo@yahoo.fr>
2023-02-02 17:19:47 +0100

    added last project

--- src/data/ProjectsList.jsx
+++ src/data/ProjectsList.jsx
@@ -16,21 +16,76 @@ import LearnroomJoin from "../assets/images/LearnroomJoin.png"
 import LearnroomTeam from "../assets/images/LearnroomTeam.png"
 import LearnroomProfile from "../assets/images/LearnroomProfile.png"
 import portofoliov1API from "../assets/images/portofoliov1API.png"
+
+import blogPerf from "../assets/images/blog/blogPerf.png"
+import blogDessins from "../assets/images/blog/blogDessins.png"
+import blogDessins2 from "../assets/images/blog/blogDessins2.png"
+import blogIndex from "../assets/images/blog/blogIndex.png"
+import blogIndex3 from "../assets/images/blog/blogIndex3.png"
+import blogLogin from "../assets/images/blog/blogLogin.png"
+import blogNewsletter1 from "../assets/images/blog/blogNewsletter1.png"
+import blogNewsletter2 from "../assets/images/blog/blogNewsletter2.png"
+import blogNewsletter3 from "../assets/images/blog/blogNewsletter3.png"
+
 export const ProjectsList = [
   {
     title: "Blog automatisé",
     ref: "blog",
     link: "https://site-temporaire-blog-de-gaelle.netlify.app/",
     code: "https://github.com/HugoCalmels/blog",
-    image: Clonebook,
+    image: blogDessins,
     shortDescription: "Une application pour poster des photos/vidéos qui a pour vocation d'être un site de publication d'oeuvres d'art.",
-    longDescription: "this description is actually the long description which makes it way longer for the reason it's the long desctiption",
+    longDescription: `Fraîchement réalisé, ceci n'est pas la version finale du site c'est une MVP ( Minimum Viable Product ). C'est une application qui a pour but d'aider le client ( ma mère ) à poster du contenu sur son site, sans n'y connaître rien en développement informatique. Elle est néamoins très exigeante, et moi aussi. Ce site est conforme au RGPD concernant la newsletter, et par conséquent le stockage d'adresses email. `,
     tags: ["React.Js", "Rails API", "JavaScript", "JWT-tokens", "PostgreSQL", "AWS"],
     features: [
       {
-        screenshot: Clonebook,
-        description: "no feature description yet"
-      }
+        title: "1.1 - Index du site",
+        screenshot: blogIndex,
+        description: `Voici un apperçu de la page index.
+        `
+      },
+      {
+        title: "1.2 - Index du site en admin",
+        screenshot: blogIndex3,
+        description: `Le principe de cette application, est de rendre un maximum de choses modulables et modifiables.
+        `
+      },
+      {
+        title: "2.1 - Partie administrateur",
+        screenshot: blogLogin,
+        description: `L'admin devra s'authentifier. Chaque requête de changement de contenu du site est protégée par un token généré aléatoirement et regénéré à chaque nouvelle connexion.
+        `
+      },
+      {
+        title: "2.2 - Page dessins en admin",
+        screenshot: blogDessins2,
+        description: `L'admin pourra publier/éditer/supprimer des catégories, et il pourra publier/éditer/supprimer des fichiers images appartenant à ces catégories. Il n'y a aucun changement de page lors des manipulations, ce qui rend l'expérience utilisateur agréable.
+        `
+      },
+      {
+        title: "2.3 - Page performances en admin",
+        screenshot: blogPerf,
+        description: `L'admin pourra publier/éditer/supprimer des catégories, et il pourra publier/éditer/supprimer des fichiers vidéos appartenant à ces catégories, ainsi qu'un lien vers un site existant.
+        `
+      },
+      {
+        title: "3.1 - Newsletter",
+        screenshot: blogNewsletter1,
+        description: `Chaque utilisateur a la possiblité de s'abonner à la newsletter du site. Il a aussi la possibilité de se désabonner avec un lien présent dans chaque mail de newsletter.
+        `
+      },
+      {
+        title: "3.2 - Newsletter",
+        screenshot: blogNewsletter2,
+        description: `Voici à quoi ressemble la base de données actuelle concernant la newsletter. Les addresses email qui recevront chaque newsletter envoyée depuis le site.
+        `
+      },
+      {
+        title: "3.3 - Newsletter",
+        screenshot: blogNewsletter3,
+        description: `L'admin devra créer son template sur un site tiers, et copier le code HTML du template dans le champ prévu à cet effet. Une prévisualisation est présente pour voir à quoi ressemblera l'email.
+        `
+      },
     ]
   },
   {
@@ -40,7 +95,7 @@ export const ProjectsList = [
     code: "https://github.com/HugoCalmels/react-social-network-redux",
     image: Clonebook,
     shortDescription: "Un clone de Facebook, avec quelques fonctionnalités complexes.",
-    longDescription: "J'ai copié plusieurs fonctionnalité de Facebook, ainsi que son interface. Le but de ce projet était de pratiquer sur React.js en utilisant Redux, et en générant beaucoup de data. Cela m'a aussi permis de recopier et de comprendre un design qui a fait ses preuves. Cette application est une single page application ( SPA ). J'ai également crée ma propre API pour gérer les fonctionnalités, et la base de données est en PostgreSQL. ",
+    longDescription: "J'ai copié plusieurs fonctionnalité de Facebook, ainsi que son interface. Le but de ce projet était de pratiquer sur React.js en utilisant Redux, et en générant beaucoup de data. Cela m'a aussi permis de recopier et de comprendre un design qui a fait ses preuves.",
     tags: ["React.Js", "Rails API", "JavaScript", "Redux-toolkit", "PostgreSQL", "AWS"],
     features: [
       {
@@ -83,7 +138,7 @@ export const ProjectsList = [
     code: "https://github.com/HugoCalmels/JS_Portofolio",
     image: portofoliov1,
     shortDescription: "Un portofolio que j'ai jugé trop peu professionel au niveau du design.",
-    longDescription: `En avril 2022 je n'avais pas copié encore beaucoup de maquettes ou de sites existants. Il est propre, certains l'ont trouvé "sympa", mais je suis capable aujourd'hui d'apporter un design beaucoup plus professionel. Chacun de mes portofolios ont été réalisés de A à Z en moins de 10 jours, de la maquette à la mise en production. Aussi mon nouveau portofolio est plus modulable que l'ancien, et j'aurais plus de facilités à le faire évoluer par raport à l'ancien.`,
+    longDescription: `Réalisé en avril 2022, c'est mon ancien portofolio. Depuis j'ai relevé mon niveau en design, en apportant une touche plus professionnelle. Mon nouveau portofolio est également moins complexe tant au niveau du code que de son architecture. `,
     tags: ["JavaScript", "Google API"],
     features: [
       {
@@ -137,7 +192,7 @@ export const ProjectsList = [
     code: "https://github.com/HugoCalmels/front-learn-room",
     image: LearnroomHome,
     shortDescription: `Une application pour créer et rejoindre des sessions. Elle a pour but de rassembler les utilisateurs afin qu'ils étudient ensemble sur les mêmes fuseaux horaires.`,
-    longDescription: `Mon dernier projet de groupe, lors du bootcamp "The Hacking Project", réalisé avec trois autres camarades. J'ai participé à la mise en place de l'API incluant l'authentication, la récupération de mot passe, et les emails envoyés. Sur l'interface ( le front ), j'étais en charge de la page profil, mais j'ai participé sur toutes les pages. C'est une application pour rejoindre des sessions affiliées, que l'on appelle des "MOOC" ( un MOOC est un cours d'enseignement diffusé sur internet ). Nous avons aussi mis en place un système de fuseaux horaires.`,
+    longDescription: `Mon dernier projet de groupe, lors du bootcamp "The Hacking Project", réalisé avec trois autres camarades. J'ai participé à la mise en place de l'API incluant l'authentication, la récupération de mot passe, et les emails envoyés. Sur l'interface ( le Front ), j'étais en charge de la page profil, mais j'ai participé sur toutes les pages. C'est une application pour rejoindre des sessions affiliées, à ce que l'on appelle des "MOOC" ( un MOOC est un cours d'enseignement diffusé sur internet ). Nous avons aussi mis en place un système de fuseaux horaires.`,
     tags: ["React.Js", "Rails API", "JavaScript", "Redux", "JWT-tokens", "PostgreSQL"],
     features: [
       {
