6ec5276247329d72f10193274dbe96c783557e07
Hugo Calmels <calmels.hugo@yahoo.fr>
2025-03-31 14:29:44 +0200

    azdazazd

--- src/pages/sections/home/Home.jsx
+++ src/pages/sections/home/Home.jsx
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import Hero from "./Hero";
 import hugo from "../../../assets/images/hugoo.jpg";
+import mam from "../../../assets/images/mam.jpg";
 
 const Home = (props) => {
   return (
@@ -31,15 +32,17 @@ const Home = (props) => {
           <div className="ht-unit">
             <div className="ht-header">
               <div className="ht-header-image">
-                <img src={hugo}/>
+                <img src={mam}/>
               </div>
               <div className="ht-header-content">
-                <p><strong>Hugo Calmels</strong></p>
-                <p>Développeur</p>
+                <p><strong>Gaëlle Boucherit</strong></p>
+                <p>Artiste</p>
               </div>
             </div>
   
-          <p className="ht-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum sed ipsam in expedita rem numquam esse ea tempore tempora dolores reiciendis doloremque, quae laborum nostrum magnam doloribus accusantium nisi omnis?</p>
+          <p className="ht-text">J'avais besoin d un nouveau site.
+Des échanges clairs et précis avec une écoute constructive et adaptée à mes besoins.
+Aujourd’hui mon site est très performant et harmonieux. Toutes les pages sont réussies. Design élégant de la galerie et super idée pour la page d’accueil ! </p>
           </div>
         </div>
 
